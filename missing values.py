545619e5fd9035a80ca513ef36351b33f70ee598
4eADsH0t <54810680+4eADsH0t@users.noreply.github.com>
2020-08-14 01:56:19 +0530

    Add files via upload

--- missing values.py
+++ missing values.py
@@ -6,3 +6,23 @@ Created on Fri Aug 14 00:13:13 2020
 """
 
 
+import pandas as pd
+df = pd.read_csv('train.csv')
+df.isnull().sum()
+df[df['Embarked'].isnull()]
+import numpy as np
+df['Cabin_null'] = np.where(df['Cabin'].isnull(),1,0)
+
+
+df.groupby(['Survived'])['Cabin_null'].mean()
+df = pd.read_csv('Train.csv',usecols = ['Age','Fare','Survived'])
+
+df.head()
+df.isnull().mean()
+def impute_nan(df,variable,median):
+    df[variable+'_median'] = df[variable].fillna(median)
+
+median = df.Age.median()
+median
+
+impute_nan(df, 'Age', median)
